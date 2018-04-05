6498318cb41613716d08c8528623d24c96e33c5d
ldgrp <leoorpilla3@gmail.com>
2018-04-05 18:39:51 +1000

    Add option to read history from file

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from course import get_course_info, course_can_be_taken
+from course import get_course_info, course_can_be_taken, is_course_code
+
+import csv
+
+PATH_TO_HISTORY = "history.csv"
 
 def input_course_history():
     history = []
@@ -10,9 +14,26 @@ def input_course_history():
         history.append(course)
     return history
 
+def read_history_from_file():
+    with open(PATH_TO_HISTORY) as historyfile:
+        historyreader = csv.reader(historyfile)
+        history = [row[0] for row in historyreader]
+        history = [row for row in history if is_course_code(row)]
+    return history
+
 def main():
-    history = input_course_history()    
-    
+    print("1) Manual input")
+    print("2) Read from file.")
+
+    choice = input("Input: ")
+    history = []
+    if choice == '1':
+        history = input_course_history()    
+    elif choice == '2':
+        history = read_history_from_file()
+
+    print(history)
+
     print("\n\nWhat course do you want to take?")
     course = input("Course Code: ")
 
