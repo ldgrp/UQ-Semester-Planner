1d21befb21cf2fab4b93c8f0a3cd1c4dea35ecbf
Leo Orpilla III <leoorpilla3@gmail.com>
2018-04-06 18:55:07 +1000

    Expose course_can_be_found to api endpoint

--- uqsemplanner/routes.py
+++ uqsemplanner/routes.py
@@ -1,6 +1,8 @@
 from uqsemplanner import app
+from uqsemplanner.course import course_can_be_taken, get_course_info
 from uqsemplanner.database import get_course_title
-from flask import abort
+
+from flask import abort, request
 from flask_restful import Api, Resource
 
 api = Api(app, catch_all_404s=True)
@@ -11,6 +13,16 @@ class Course(Resource):
         title = get_course_title(code)
         return {'code': code, 'title': title}
 
-    def put(self, code):
-        return {code: code}
+    def post(self, code):
+        history = None
+        code = code.upper()
+        req_data = request.get_json(force=True)
+        if 'history' in req_data:
+            history = req_data['history']
+        else:
+            abort(400)
+        info = get_course_info(code)
+        res = course_can_be_taken(info, history)
+        return {'course_can_be_taken': res}
+
 api.add_resource(Course, '/api/<string:code>')
