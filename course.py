291a939315d017087510a6a09018f91c550dde23
Leo Orpilla III <leoorpilla3@gmail.com>
2018-04-07 21:50:50 +1000

    Improve Course design
    Implented PrerequisiteCondition and IncompatibleCondition as subclasses
    of CourseCondition.
    
    Move logic into classes.

--- course.py
+++ course.py
@@ -29,13 +29,60 @@ class Course:
     def set_title(self, title):
         self.title = title
 
+    def can_be_taken(self, history):
+        if self.incompatible.evaluate(self, history):
+            return False
+        return self.prerequisite.evaluate(self, history)
+
+class PrerequisiteCondition(CourseCondition):
+    def __init__(self, raw_condition):
+        super(self, raw_condition)
+
+    def evaluate(self, history):
+        courses = [course for course in self.courses if course not in history]
+        courses = {course: False for course in courses}
+        history = {course: True for course in history}
+        history.update(courses)
+
+        result = eval(expr, {}, history) # WARNING: DANGEROUS FUNCTION
+        return result
+
+class IncompatibleCondition(CourseCondition):
+    def __init__(self, raw_condition):
+        super(self, raw_condition)
+
+    def evaluate(self, history):
+        for course in self.courses:
+            if course in history:
+                return True
+        return False
+        
 class CourseCondition:
     def __init__(self, raw_condition):
         self.raw_condition = raw_condition
-        self.process()
+        self.condition = None
+        self.courses = None
 
-    def process():
-        pass
+        self.parse_raw_condition()
+
+    def parse_raw_condition(self):
+        condition = self.raw_condition
+        condition = condition.replace(',', ' or ')
+        condition = condition.replace('+', ' and ')
+    
+        #words = re.findall(r"[\w']+", info)
+        #courses = []
+
+        #for word in words:
+        #    if is_course_code(word):
+        #        courses.append(word)
+        #    elif word not in ['or', 'and']:
+        #        raise ValueError("Unexpected word '" + word + "'  while parsing course info.")
+        self.condition = condition
+        self.courses = None
+
+    def evaluate(self, history):
+        raise NotImplementedError()
 
 def scrape_course_info(course):
     soup = get_soup(URL + course.code)
@@ -51,9 +98,9 @@ def scrape_course_info(course):
     ]
     conditions = [soup.find(id=condition).get_text(strip=True) for condition in conditions]
 
-    incompatibles = CourseCondition(condition[0])
-    prerequisites = CourseCondition(condition[1])
-    r_prerequisites = CourseCondition(condition[2])
+    incompatibles = IncompatibleCondition(condition[0])
+    prerequisites = PrerequisiteCondition(condition[1])
+    r_prerequisites = PrerequisiteCondition(condition[2])
 
     title = soup.find(id='course-title').get_text(strip=True)
     course.set_title(title)
@@ -64,50 +111,7 @@ def scrape_course_info(course):
 
     return course
 
-def parse_course_info(info):
-    info = info.replace(',', ' or ')
-    info = info.replace('+', ' and ')
-
-    words = re.findall(r"[\w']+", info)
-    courses = []
-
-    for word in words:
-        if is_course_code(word):
-            courses.append(word)
-        elif word not in ['or', 'and']:
-            raise ValueError("Unexpected word '" + word + "'  while parsing course info.")
-    return info, courses 
-
-def satisfies_prerequisite(prerequisite, history):
-    expr, courses = parse_course_info(prerequisite)
-    
-    courses = [course for course in courses if course not in history]
-    courses = {course: False for course in courses}
-    history = {course: True for course in history}
-    history.update(courses)
-
-    result = eval(expr, {}, history) # WARNING: DANGEROUS FUNCTION
-    return result
-
-def is_incompatible(incompatible, history):
-    _,courses = parse_course_info(incompatible)
-    for course in courses:
-        if course in history:
-            return True 
-    return False
-
-def course_can_be_taken(info, history):
-    prerequisite = info["prerequisite"]
-    incompatible = info["incompatible"]
-    
-    if is_incompatible(incompatible, history):
-        return False
-    
-    return satisfies_prerequisite(prerequisite, history)
-    
-    
 def is_course_code(string):
-    return string in COURSE_CODES
-    # return len(string) == 8 and \
-    #        string[:4].isalpha() and \
-    #        string[4:8].isdigit()
+     return len(string) == 8 and \
+            string[:4].isalpha() and \
+            string[4:8].isdigit()
