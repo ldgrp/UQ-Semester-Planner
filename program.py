83a04be93cabf74a0ddc74a8038a11b2a5e727e2
Leo Orpilla III <leoorpilla3@gmail.com>
2018-04-07 21:00:24 +1000

    Add feature to scrape plans with programs

--- program.py
+++ program.py
@@ -3,19 +3,58 @@ import re
 
 URL = 'https://my.uq.edu.au/programs-courses/browse.html?level=ugpg' 
 
+class Program:
+    def __init__(self, code, title):
+        self.code = code
+        self.title = title
+        self.plans = []
+
+    def add_plan(self, plan):
+        self.plans.append(plan)
+
+    def add_plans(self, plans):
+        for plan in plans:
+            self.add_plan(plan)
+
+    def get_plans(self):
+        return self.plans
+
+class Plan:
+    def __init__(self, code, title):
+        self.code = code
+        self.title = title
+
 def scrape():
-    programs = {}
+    programs = []
+    plans = [] 
 
     soup = get_soup(URL) 
-    soup = soup.findAll('a', href=re.compile('acad_prog'))
-    
-    for a in soup:
+    a_acad_prog = soup.findAll('a', href=re.compile('acad_prog'))
+    a_acad_plan = soup.findAll('a', href=re.compile('acad_plan'))
+
+    for a in a_acad_plan:
         url = a['href']
         code = url[url.index('=')+1:]
         title = a.get_text(strip=True)
-        programs[code] = title
+        plan = Plan(code, title)
+        plans.append(plan)
+
+    for a in a_acad_prog:
+        url = a['href']
+        code = url[url.index('=')+1:]
+        title = a.get_text(strip=True)
+
+        program = Program(code, title)
+
+        majors = [plan for plan in plans if code in plan.code]
+        program.add_plans(majors)
+        programs.append(program)
 
     return programs
 
 if __name__ == "__main__":
-    print(scrape())
+    programs = scrape()
+    for program in programs:
+        print("{}-{}".format(program.code, program.title))
+        for plan in program.get_plans():
+            print("\t{}".format(plan.title))
