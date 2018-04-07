c9dd21fe09a6ea0eb293e7bd2cd17b3865db61f0
Leo Orpilla III <leoorpilla3@gmail.com>
2018-04-07 22:25:07 +1000

    Add course scraper

--- course.py
+++ course.py
@@ -2,7 +2,8 @@ from helpers import get_soup
 import csv
 import re
 
-URL = "https://my.uq.edu.au/programs-courses/course.html?course_code="
+COURSE_URL = "https://my.uq.edu.au/programs-courses/course.html?course_code="
+COURSE_CATALOG_URL = "https://my.uq.edu.au/programs-courses/search.html?keywords=+&searchType=all&archived=true&CourseParameters%5Bsemester%5D=#courses"
 
 class Course:
     def __init__(self, code, title=None):
@@ -78,8 +79,23 @@ class CourseCondition:
     def evaluate(self, history):
         raise NotImplementedError()
 
+def scrape():
+    soup = get_soup(COURSE_CATALOG_URL)
+
+    courses = []
+    codes = soup.findAll('a', 'code')
+
+    for code in codes:
+        name = code.nextSibling.nextSibling.get_text(strip=True)
+        course_code = code.get_text(strip=True)
+        
+        course = Course(course_code, name)
+        courses.append(course)
+
+    return courses
+
 def scrape_course_info(course):
-    soup = get_soup(URL + course.code)
+    soup = get_soup(COURSE_URL + course.code)
 
     if soup.find(id="course-notfound"):
         raise ValueError("Course code not found")
