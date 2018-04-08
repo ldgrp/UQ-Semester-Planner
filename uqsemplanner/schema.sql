c7ad2e375614095edc1b9ab659b95996c5d4f51c
Leo Orpilla III <leoorpilla3@gmail.com>
2018-04-08 22:29:16 +1000

    Add program and major to schema

--- uqsemplanner/schema.sql
+++ uqsemplanner/schema.sql
@@ -1,5 +1,21 @@
 drop table if exists courses;
 create table courses (
     'code' text primary key not null,
+    title text not null,
+    prerequisite text,
+    incompatible text,
+    recommended_prerequisite text
+);
+
+create table programs (
+    'code' text primary key not null,
     title text not null
 );
+
+create table majors (
+    'code' text primary key not null,
+    title text not null,
+    pcode text references programs ('code')
+);
+
+
